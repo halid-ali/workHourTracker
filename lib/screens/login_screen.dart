5e9a58849ff50410ddbb32eb8d58be7470174f03
Halid Ali <halid.ali@methodpark.de>
2021-03-06 22:09:16 +0100

    fetch data from firestore

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:work_hour_tracker/routes.dart';
 
@@ -11,15 +12,32 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
     return SafeArea(
       child: Scaffold(
         body: Center(
-          child: Container(
-            child: TextButton(
-              child: Text('Login Screen'),
-              onPressed: () =>
-                  Navigator.pushNamed(context, RouteGenerator.homePage),
-            ),
+          child: StreamBuilder(
+            stream: firestore.collection('users').snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return Text('Error occured');
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+
+              var users = snapshot.data.docs;
+              return ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: EdgeInsets.all(15),
+                    child: Text(users[index].data()['username']),
+                  );
+                },
+              );
+            },
           ),
         ),
       ),
