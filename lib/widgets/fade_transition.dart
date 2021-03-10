e83ff173d6ad4f3c35c36511c0dc8562c62513fb
Halid Ali <halid.ali@methodpark.de>
2021-03-10 09:51:48 +0100

    fade transition between screens

---
+++ lib/widgets/fade_transition.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class FadeRouteTransition extends PageRouteBuilder {
+  final Widget page;
+
+  FadeRouteTransition({this.page})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              FadeTransition(
+            opacity: animation,
+            child: child,
+          ),
+        );
+}
